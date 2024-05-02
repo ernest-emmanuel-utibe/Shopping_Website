d91666cfdead0104813d5d14780b0048d68f78c8
ernest-emmanuel-utibe <em123nest@gmail.com>
2024-05-02 09:47:00 +0100

    feat: Fixing bugs

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import { BiEnvelope } from 'react-icons/bi';
+import { FaEnvelopesBulk } from "react-icons/fa6";
 
 const Footer = () => {
+  const year = new Date().getFullYear();
+
   return (
-    <div className="bg-blackish text-gray-500 text-center py-4 pb-16 md:pb-4">
-        Copyright © Logo <br /> All Rights Reserved 2023.
-        </div>
+
+    <div className="bg-gray text-gray-500 text-center py-4 pb-16 md:pb-4">
+        © Pera {year}<br /> All Rights Reserved 2023
+          info@pera_gold.com.ng
+    </div>
   )
 }
 
--- src/components/Slide.tsx
+++ src/components/Slide.tsx
@@ -10,7 +10,7 @@ interface propsType {
 
 const Slide:React.FC<propsType> = ({img, title, mainTitle, price}) => {
   return (
-    <div className="outline-none border-none relative">
+    <div className="outline-none border-none relative h-[80vh]">
         <div className="absolute left[30px] md:left-[70px] max-w-[250px] sm:max-w-[350px] top-[50%]
         -translate-y-[50%] space-y-2 lg:space-y-4 bg-[#ffffffa2] sm:bg-transparent p-4 sm:p-0 rounded-lg
         sm:rounded-none">
@@ -31,7 +31,7 @@ const Slide:React.FC<propsType> = ({img, title, mainTitle, price}) => {
             </div>
         </div>
 
-        <Image className="w-[100%] h-[300px] md:h-auto rounded-xl object-cover object-right md:object-left-bottom"
+        <Image className="w-full h-full rounded-xl object-cover object-right md:object-left-bottom"
         src={img}
         alt="banner"
         width={2000}
